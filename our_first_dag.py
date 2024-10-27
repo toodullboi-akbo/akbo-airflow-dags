7166018475bb77be9669a264bf5cc00662ff4e90
minguk-cucu <74689888+minguk-cucu@users.noreply.github.com>
2024-10-27 10:53:40 +0900

    Update our_first_dag.py

--- our_first_dag.py
+++ our_first_dag.py
@@ -13,8 +13,6 @@ with DAG(
     dag_id = "our_first_dag",
     default_args=default_args,
     description = "This is mine",
-    start_date = datetime(2024, 10, 7, 2),
-    schedule_interval="@daily"
 ) as dag:
     task1 = BashOperator(
         task_id = "first_task",
