06193123b069fe6df451fe94c15fafed184f6abb
minguk-cucu <74689888+minguk-cucu@users.noreply.github.com>
2024-10-29 12:02:22 +0900

    Update our_first_dag.py

--- our_first_dag.py
+++ our_first_dag.py
@@ -16,7 +16,7 @@ with DAG(
 ) as dag:
     task1 = BashOperator(
         task_id = "first_task",
-        bash_command="ls"
+        bash_command="python /opt/airflow/dags/src/printSomethingTest.py"
     )
 
     task1
