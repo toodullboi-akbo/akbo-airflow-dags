96b22e25002aa865c280cf80f888dfc2dfb8493b
minguk-cucu <minkuk0118@gmail.com>
2024-12-04 14:33:28 +0900

    scala jar test

--- spark_test_dag.py
+++ spark_test_dag.py
@@ -18,21 +18,29 @@ with DAG(
     # Define the SparkSubmitOperator task
     submit_spark_job = SparkSubmitOperator(
         task_id='submit_spark_job',
-        application="wasbs://spark-jars@"+os.getenv("AIRFLOW_STORAGE_ACCOUNT_NAME")+".blob.core.windows.net/scala-dustmq_2.13-0.1.1-SNAPSHOT.jar",
+        application="wasbs://spark-jars@"+os.getenv("AIRFLOW_STORAGE_ACCOUNT_NAME")+".blob.core.windows.net/scala-dustmq_2.13-0.1.2-SNAPSHOT.jar",
         # '/opt/airflow/dags/repo/src/spark/scala-dustmq_2.13-0.1.1-SNAPSHOT.jar',  
         conn_id='spark_cluster_connection',
         java_class='dustmq', 
         # application_args=['arg1', 'arg2'],  
         conf={
             'spark.executor.memory': '2g', 'spark.executor.cores': '1',
-            "fs.azure.account.key."+os.getenv("AIRFLOW_STORAGE_ACCOUNT_NAME")+".blob.core.windows.net": os.getenv('AIRFLOW_STORAGE_ACCOUNT_KEY'),
+            f"spark.hadoop.fs.azure.account.key.{os.getenv("AIRFLOW_STORAGE_ACCOUNT_NAME")}.blob.core.windows.net": os.getenv('AIRFLOW_STORAGE_ACCOUNT_KEY'),
         },  
         name='spark_airflow_job',
         verbose=True,
-        packages='com.azure:azure-storage-blob:12.25.0,com.azure:azure-identity:1.11.2,'+
-                'com.microsoft.azure:azure-storage:8.6.6,org.apache.hadoop:hadoop-azure:3.3.6,org.apache.hadoop:hadoop-azure-datalake:3.3.6,'+
-                'org.apache.hadoop:hadoop-common:3.3.6,org.apache.hadoop:hadoop-client:3.3.6,org.apache.hadoop:hadoop-client-api:3.3.6,'+
-                'org.apache.hadoop:hadoop-client-runtime:3.3.6' 
+        packages=','.join([
+            'com.azure:azure-storage-blob:12.25.0',
+            'com.azure:azure-identity:1.11.2',
+            'com.microsoft.azure:azure-storage:8.6.6',
+            'org.apache.hadoop:hadoop-azure:3.3.6',
+            'org.apache.hadoop:hadoop-azure-datalake:3.3.6',
+            'org.apache.hadoop:hadoop-common:3.3.6',
+            'org.apache.hadoop:hadoop-client:3.3.6',
+            'org.apache.hadoop:hadoop-client-api:3.3.6',
+            'org.apache.hadoop:hadoop-client-runtime:3.3.6',
+        ])
+        
     )
 
 
