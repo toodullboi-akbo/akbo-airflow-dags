56289ea3efca4f50083292ad898330ff28532f9e
minguk-cucu <74689888+minguk-cucu@users.noreply.github.com>
2025-01-15 16:27:03 +0900

    Create tester_dag.py

---
+++ tester_dag.py
@@ -0,0 +1,23 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.empty import EmptyOperator
+
+from datetime import datetime, timedelta
+
+default_args = {
+    "owner" : "toodullboi",
+}
+
+with DAG(
+    dag_id = "tester",
+    default_args=default_args,
+    description = "test whatever we want",
+) as dag:
+    testTask = BashOperator(
+        task_id = "ls test",
+        bash_command="ls"
+    )
+
+
+
+    testTask
