7eae4d07507f455602004a12349683a657ee51f1
minguk-cucu <minkuk0118@gmail.com>
2025-01-17 22:31:52 +0900

    add daily & situation dag

---
+++ KBO_entire_situaion_daily_dag.py
@@ -0,0 +1,46 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.empty import EmptyOperator
+
+from datetime import datetime, timedelta
+
+default_args = {
+    "owner" : "toodullboi",
+    "retries" : 5,
+    "retry_delay" : timedelta(minutes=5)
+}
+
+with DAG(
+    dag_id = "Entire_situation_daily_dag",
+    default_args=default_args,
+    description = "crawls situation&daily data",
+) as dag:
+
+    entire_batter_daily_task = BashOperator(
+        task_id = "entire_batter_daily",
+        bash_command="python /opt/airflow/dags/repo/src/entire/entire_batter_daily.py"
+    )
+
+    entire_batter_situation_task = BashOperator(
+        task_id = "entire_batter_situation",
+        bash_command="python /opt/airflow/dags/repo/src/entire/entire_batter_situation.py"
+    )
+
+    entire_pitcher_daily_task = BashOperator(
+        task_id = "entire_pitcher_daily",
+        bash_command="python /opt/airflow/dags/repo/src/entire/entire_pitcher_daily.py"
+    )
+    
+    entire_pitcher_situation_task = BashOperator(
+        task_id = "entire_pitcher_situation",
+        bash_command="python /opt/airflow/dags/repo/src/entire/entire_pitcher_situation.py"
+    )
+
+    startTask = EmptyOperator(task_id="stark_task")
+
+
+    startTask >> [entire_batter_situation_task, entire_pitcher_situation_task]
+    entire_batter_situation_task >> entire_batter_daily_task
+    entire_pitcher_situation_task >> entire_pitcher_daily_task
+
+
